48404a9dd813f046e2a55d4715d02751596c8c72
falconlaravguru <falcon.laravguru@yandex.com>
2017-07-28 07:21:26 +1200

    db-3

--- config/database.php
+++ config/database.php
@@ -32,13 +32,13 @@ return [
     */
 
     'connections' => [
-
+        
         'heroku' => [  
             'driver'   => 'pgsql',
             'host'     => parse_url(getenv("DATABASE_URL"))["host"],
             'database' => substr(parse_url(getenv("DATABASE_URL"))["path"], 1),
-            'username' => parse_url(getenv("DB_USERNAME")),
-            'password' => parse_url(getenv("DB_PASSWORD")),
+            'username' => parse_url(getenv("DATABASE_URL"))["user"],
+            'password' => parse_url(getenv("DATABASE_URL"))["pass"],
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
